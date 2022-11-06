e07bc9defdcf635c109c52b40aa846b8c3b544e0
Mauricio-Carrion <mauricio.sjn@gmail.com>
2022-11-06 19:28:40 -0300

    Updates

---
+++ precache-manifest.6b3d36bac6c182f469229ef0fc5bfa01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3901dc3f0a0df1ec7ca9",
+    "url": "/CalculadoraReact/static/css/main.09e69bde.chunk.css"
+  },
+  {
+    "revision": "3901dc3f0a0df1ec7ca9",
+    "url": "/CalculadoraReact/static/js/main.3901dc3f.chunk.js"
+  },
+  {
+    "revision": "ce9c03ff9d8e3d6050fc",
+    "url": "/CalculadoraReact/static/js/1.ce9c03ff.chunk.js"
+  },
+  {
+    "revision": "25919faeca0ebeafbd53",
+    "url": "/CalculadoraReact/static/js/runtime~main.25919fae.js"
+  },
+  {
+    "revision": "d1a7b45f28bf337cab8adf3992f669a0",
+    "url": "/CalculadoraReact/static/media/RobotoMono-Thin.d1a7b45f.ttf"
+  },
+  {
+    "revision": "f92b5213fa459accb76609b090dc63dd",
+    "url": "/CalculadoraReact/index.html"
+  }
+];
